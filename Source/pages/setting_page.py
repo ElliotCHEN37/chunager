0e51f68cb5817d3223d606620f6676481bdd6bf6
Elliot <elliotchen2010@gmail.com>
2025-05-24 13:55:47 +0800

    取消儲存按鈕，設定可以即時儲存

--- Source/pages/setting_page.py
+++ Source/pages/setting_page.py
@@ -5,8 +5,7 @@ import webbrowser
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QFileDialog, QHBoxLayout
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont, QIcon
-from qfluentwidgets import ComboBox, StrongBodyLabel, TitleLabel, PrimaryPushButton, Flyout, InfoBarIcon, \
-    FlyoutAnimationType, LineEdit, PushButton
+from qfluentwidgets import ComboBox, StrongBodyLabel, TitleLabel, LineEdit, PrimaryPushButton
 
 
 def get_path(rel_path: str) -> str:
@@ -31,45 +30,35 @@ class SettingPage(QWidget):
         font.setPointSize(20)
         font.setBold(True)
         title.setFont(font)
-        title.setAlignment(Qt.AlignLeft)
         layout.addWidget(title)
         layout.addSpacing(10)
 
-        update_lbl = StrongBodyLabel("檢查更新：")
-        self.update_btn = PushButton(QIcon(get_path("img/web.svg")), "訪問GitHub發佈頁")
-        self.update_btn.clicked.connect(self.open_repo)
-        layout.addWidget(update_lbl)
-        layout.addWidget(self.update_btn)
+        layout.addWidget(StrongBodyLabel("檢查更新："))
+        update_btn = PrimaryPushButton(QIcon(get_path("img/web.svg")), "訪問GitHub發佈頁")
+        update_btn.clicked.connect(self.open_repo)
+        layout.addWidget(update_btn)
         layout.addSpacing(10)
 
-        theme_lbl = StrongBodyLabel("選擇主題：")
+        layout.addWidget(StrongBodyLabel("選擇主題："))
         self.theme_box = ComboBox(self)
         self.theme_box.addItems(["AUTO", "DARK", "LIGHT"])
-        theme = self.cfg.get("DISPLAY", "theme", fallback="AUTO")
-        self.theme_box.setCurrentText(theme)
-        self.theme_box.currentTextChanged.connect(self.set_theme)
-        layout.addWidget(theme_lbl)
+        self.theme_box.setCurrentText(self.cfg.get("DISPLAY", "theme", fallback="AUTO"))
+        self.theme_box.currentTextChanged.connect(self.update_theme)
         layout.addWidget(self.theme_box)
         layout.addSpacing(10)
 
-        st_lbl = StrongBodyLabel("選擇 segatools.ini 路徑：")
+        layout.addWidget(StrongBodyLabel("選擇 segatools.ini 路徑："))
         st_layout = QHBoxLayout()
         self.st_path = LineEdit(self)
-        path = self.cfg.get("GENERAL", "segatools_path", fallback="")
-        self.st_path.setText(path)
+        self.st_path.setText(self.cfg.get("GENERAL", "segatools_path", fallback=""))
+        self.st_path.textChanged.connect(self.update_segatools_path)
 
-        self.st_btn = PrimaryPushButton(QIcon(get_path("img/folder.svg")), "選擇檔案")
-        self.st_btn.clicked.connect(self.pick_st_path)
+        st_btn = PrimaryPushButton(QIcon(get_path("img/folder.svg")), "選擇檔案")
+        st_btn.clicked.connect(self.pick_st_path)
 
         st_layout.addWidget(self.st_path)
-        st_layout.addWidget(self.st_btn)
-        layout.addWidget(st_lbl)
+        st_layout.addWidget(st_btn)
         layout.addLayout(st_layout)
-        layout.addSpacing(10)
-
-        self.save_btn = PrimaryPushButton(QIcon(get_path("img/save.svg")), "儲存設定")
-        self.save_btn.clicked.connect(self.save_cfg)
-        layout.addWidget(self.save_btn)
 
     def get_cfg_path(self):
         if getattr(sys, 'frozen', False):
@@ -81,33 +70,28 @@ class SettingPage(QWidget):
     def load_cfg(self):
         cfg = configparser.ConfigParser()
         cfg.read(self.cfg_path, encoding="utf-8")
+        if not cfg.has_section("DISPLAY"):
+            cfg.add_section("DISPLAY")
+        if not cfg.has_section("GENERAL"):
+            cfg.add_section("GENERAL")
         return cfg
 
     def save_cfg(self):
-        self.cfg.set("DISPLAY", "theme", self.theme_box.currentText())
-        self.cfg.set("GENERAL", "segatools_path", self.st_path.text())
-
         with open(self.cfg_path, "w", encoding="utf-8") as file:
             self.cfg.write(file)
 
-        Flyout.create(
-            icon=InfoBarIcon.SUCCESS,
-            title='完成',
-            content="所有選項已儲存，重新啟動以生效",
-            target=self.save_btn,
-            parent=self,
-            isClosable=True,
-            aniType=FlyoutAnimationType.PULL_UP
-        )
+    def update_theme(self, text):
+        self.cfg.set("DISPLAY", "theme", text)
+        self.save_cfg()
+
+    def update_segatools_path(self, text):
+        self.cfg.set("GENERAL", "segatools_path", text)
+        self.save_cfg()
 
     def pick_st_path(self):
-        dialog = QFileDialog(self)
-        path, _ = dialog.getOpenFileName(self, "選擇 segatools.ini", "", "SEGATOOLS配置檔 (segatools.ini)")
+        path, _ = QFileDialog.getOpenFileName(self, "選擇 segatools.ini", "", "SEGATOOLS配置檔 (segatools.ini)")
         if path:
             self.st_path.setText(path)
-
-    def set_theme(self):
-        self.cfg.set("DISPLAY", "theme", self.theme_box.currentText())
 
     def open_repo(self):
         webbrowser.open("https://github.com/ElliotCHEN37/chunager/releases")
